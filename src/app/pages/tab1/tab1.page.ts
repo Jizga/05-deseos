c1ba1d2d4ed82cc8ea39f13347d0d204ac7266e6
Jizga <jizga93@gmail.com>
2021-01-04 20:23:15 +0100

    localStorage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -9,11 +10,46 @@ import { DeseosService } from 'src/app/services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor( public deseosService: DeseosService,
-              private router: Router ) { }
+  constructor(public deseosService: DeseosService,
+    private router: Router,
+    private alertController: AlertController) { }
 
-  agregarLista(){
-    this.router.navigateByUrl( '/tabs/tab1/agregar' )
+  async agregarLista() {
+    //this.router.navigateByUrl( '/tabs/tab1/agregar' )
+
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: ' cancel',
+        handler: () => {
+          console.log('Cancelar')
+        }
+      },
+      {
+        text: 'Crear',
+        handler: (data) => {
+          console.log(data);
+
+          if (data.titulo.lenght === 0) {
+            return;
+          }
+
+          //crear la nueva lista
+
+          this.deseosService.crearLista(data.titulo);
+        }
+      }]
+    });
+
+    alert.present();
+
   }
 
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -10,13 +10,27 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-
-    const lista1 = new Lista('Recolectar las piedras del Infinito');
-    const lista2 = new Lista('Héroe a desaparecer');
-
-    this.listas.push(lista1, lista2);
-
+    this.cargarStorage();
   }
 
+  crearLista(titulo: string) {
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas))
+  }
+
+  cargarStorage() {
+
+    if (localStorage.getItem('data')) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
+
+  }
 
 }
