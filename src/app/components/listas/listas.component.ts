040591ef54a528a36f9745ed3e569866d70e6816
Jizga <jizga93@gmail.com>
2021-01-10 19:58:14 +0100

    Editar título de una lista

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.module';
 import { DeseosService } from 'src/app/services/deseos.service';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -10,23 +11,68 @@ import { DeseosService } from 'src/app/services/deseos.service';
 })
 export class ListasComponent {
 
+
+  @ViewChild(IonList) lista: IonList;
+
   @Input() terminada = true;
 
   constructor(public deseosService: DeseosService,
-              private router: Router) { }
+    private router: Router,
+    private alertController: AlertController) { }
+
+
+
 
   listaSeleccionada(lista: Lista) {
-    if(this.terminada){
+    if (this.terminada) {
       this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`)
-    } else { 
+    } else {
       this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`)
     }
   }
 
-  borrarLista(lista: Lista){
 
-    this.deseosService.borrarLista( lista )
+  borrarLista(lista: Lista) {
+    this.deseosService.borrarLista(lista)
+  }
 
+
+  async editarLista(lista: Lista) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Editar Lista',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        value: lista.titulo,
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: ' cancel',
+        handler: () => {
+          console.log('Cancelar')
+          this.lista.closeSlidingItems();
+        }
+      },
+      {
+        text: 'Actualizar',
+        handler: (data) => {
+          console.log(data);
+
+          if (data.titulo.length === 0) {
+            return;
+          }
+
+          lista.titulo = data.titulo;
+
+          this.deseosService.guardarStorage();
+          this.lista.closeSlidingItems();
+        }
+      }]
+    });
+
+    alert.present();
   }
 
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -22,13 +22,15 @@ export class DeseosService {
   }
 
 
-  borrarLista( lista: Lista ){
+  borrarLista(lista: Lista) {
 
     this.listas = this.listas.filter(listaData => listaData.id !== lista.id)
 
     this.guardarStorage()
 
   }
+
+  
 
   obtenerLista(id: string | number) {
     id = Number(id);
